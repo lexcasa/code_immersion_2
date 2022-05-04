4e45aa960c055ccf47443abe8ec775c01e260a51
Alex Casadevall <alex@lexartlabs.com>
2022-05-03 21:22:17 -0300

    add vue app

---
+++ vue/app01/app.js
@@ -0,0 +1,11 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        mensaje: "Hola Mundo!",
+        nombre: "Alex",
+        persona: {
+            nombre: "Juan",
+            edad: 30
+        }
+    }
+})
