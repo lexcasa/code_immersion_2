d78bc1cc424dc81a8a342b2576ef122912befa1b
Alex Casadevall <alex@lexartlabs.com>
2022-05-03 21:49:31 -0300

    update app vue

--- vue/app01/app.js
+++ vue/app01/app.js
@@ -1,11 +1,17 @@
 const app = new Vue({
     el: '#app',
     data: {
-        mensaje: "Hola Mundo!",
-        nombre: "Alex",
-        persona: {
-            nombre: "Juan",
-            edad: 30
+        // Elementos HTML en un Objeto de datos de JS
+        elements: {
+            buttons: {
+                logIn: "Login",
+                signUp: "Sign Up",
+                loginSupport: "Having trouble login in?"
+            }
+        },
+        form: {
+            email: null,
+            password: null
         }
     }
 })
