46bd845ffc71d99aafbf670bf37223ae5eb7ad2f
Alex Casadevall <alex@lexartlabs.com>
2022-05-03 22:02:58 -0300

    add vue app

--- vue/app01/app.js
+++ vue/app01/app.js
@@ -1,3 +1,5 @@
+const DASHBOARD_VIEW = './dashboard.html'
+
 const app = new Vue({
     el: '#app',
     data: {
@@ -13,5 +15,32 @@ const app = new Vue({
             email: null,
             password: null
         }
+    },
+    methods: {
+        // Como obtengo los datos del form
+        // Cuando doy clic en el boton
+        obtenerDatosForm: function (){
+            console.log("formulario ::", this.form)
+
+            let validUser = validarUsuario(this.form.email, this.form.password)
+
+            if(validUser){
+                window.location.href = DASHBOARD_VIEW
+            } else {
+                alert("Error usuario y/o clave incorrecta")
+            }
+        }
     }
 })
+
+// LOGICA
+const validarUsuario = function (email, clave){
+    const EMAIL = 'alex@gmail.com'
+    const CLAVE = '123456'
+
+    if(email === EMAIL && clave === CLAVE){
+        return true
+    }
+
+    return false
+}
